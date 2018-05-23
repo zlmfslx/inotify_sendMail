910d6b1f4fb1842e87767239b1d1e0c72b9b5431
zlmfslx <10634780@qq.com>
2018-05-23 14:27:56 +0800

    this is a innrify_to_send_mail tools

---
+++ inotify_sendMail.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+# @Time    : 2018/3/20 15:48
+# @Author  : zlmfslx
+# @Oath    : Your happiness is my greatest satisfaction
+# @File    : search.py
+#参照 python search.py baidu.com 格式输入
+import sys
+
+
+reload(sys)
+sys.setdefaultencoding('UTF-8')
+
+
+lujing = '001-PhpStorm配置Git.md'
+print lujing.split('.')[0]
